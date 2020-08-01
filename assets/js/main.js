43c12929f42a797fcd3b0523c98e657e929bf485
ANikhilAgarwal <agarwalnikhil59@gmail.com>
2020-08-01 23:41:43 +0530

    index page with responsive navigation and footer created and bootstrap, fontawesone and boxicons folders also added

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// const home_svg = document.querySelectorAll("#home_svg path");
+// console.log(home_svg);
+
+// for(let i = 0; i<home_svg.length; i++) {
+//     console.log(`Letter ${i} is ${home_svg[i].getTotalLength()}`);
+// }
+
